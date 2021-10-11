29fd18f9985d423943e09a5e32063820c0536461
Yash Totla <yash.totla@oracle.com>
2021-10-12 00:40:34 +0530

    import dependencies

--- bert-qna/src/App.jsx
+++ bert-qna/src/App.jsx
@@ -1,45 +1,52 @@
-import { useState } from 'react'
+import React, { useState, useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// import dependencies
+import * as TF from "@tensorflow/tfjs"
+import * as QNA from "@tensorflow-models/qna"
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import Loader from "react-loader-spinner"
+import { Fragment } from 'react'
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+function App() {
+	const [count, setCount] = useState(0)
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>Hello Vite + React!</p>
+				<p>
+					<button type="button" onClick={() => setCount((count) => count + 1)}>
+						count is: {count}
+					</button>
+				</p>
+				<p>
+					Edit <code>App.jsx</code> and save to test HMR updates.
+				</p>
+				<p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+					{' | '}
+					<a
+						className="App-link"
+						href="https://vitejs.dev/guide/features.html"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Vite Docs
+					</a>
+				</p>
+			</header>
+		</div>
+	)
 }
 
 export default App
